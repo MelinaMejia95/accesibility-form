dc977eb3c57fbe548c2289d4116776c76e4b62b2
melinamejia95 <melinamejia95@gmail.com>
2019-09-08 16:24:51 -0500

    Menu added and scripts reorganized

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-var backdrop = document.querySelector('.backdrop');
-var modal = document.querySelector('.modal');
-var button = document.querySelector('.button');
-var closeModalButton = document.querySelector('.modal__action--negative');
-
-button.addEventListener('click', () => {
-  modal.classList.add('open');
-  backdrop.style.display = 'block';
-        setTimeout(() => {
-            backdrop.classList.add('open');
-        }, 10);
+var navButton = document.querySelector('.menu-button');
+navButton.addEventListener('click', function() {
+    let expanded = this.getAttribute('aria-expanded') === 'true' || false;
+    this.setAttribute('aria-expanded', !expanded);
+    let menu = this.nextElementSibling;
+    menu.hidden = !menu.hidden;
 });
-
-if (closeModalButton) {
-  closeModalButton.addEventListener('click', closeModal);
-}
-
-backdrop.addEventListener('click', () => {
-  this.closeModal();
-});
-
-function closeModal() {
-  if (modal) {
-      modal.classList.remove('open');
-  }
-  backdrop.classList.remove('open');
-  setTimeout(() => {
-      backdrop.style.display = 'none';
-  }, 200);
-}
