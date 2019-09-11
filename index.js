125e0b76d718d58926a8a49eebf6f4160c5a21f3
melinamejia95 <melinamejia95@gmail.com>
2019-09-10 20:34:03 -0500

    Styles added

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var navButton = document.querySelector('.menu-button');
 navButton.addEventListener('click', function() {
     let expanded = this.getAttribute('aria-expanded') === 'true' || false;
     this.setAttribute('aria-expanded', !expanded);
-    let menu = this.nextElementSibling;
+    let menu = document.querySelector('#menu-list');
     menu.hidden = !menu.hidden;
 });
--- tours/index.js
+++ tours/index.js
@@ -1,7 +1,37 @@
+
+var button = document.querySelector('.button');
+var closeModalButton = document.querySelector('.modal__action--negative');
+var modal = document.querySelector('.modal');
 var backdrop = document.querySelector('.backdrop');
+var disabledHandle, tabHandle;
+
+function openModal() {
+  modal.hidden = false;
+  backdrop.hidden = false;
+  disabledHandle = ally.maintain.disabled({
+    filter: modal,
+  });
+  tabHandle = ally.maintain.tabFocus({
+    context: modal,
+  });
+}
+
+function closeModal() {
+  modal.hidden = true;
+  backdrop.hidden = true;
+  disabledHandle.disengage();
+  tabHandle.disengage();
+}
+
+button.addEventListener('click', openModal, false);
+closeModalButton.addEventListener('click', closeModal, false);
+
+
+/*var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var button = document.querySelector('.button');
 var closeModalButton = document.querySelector('.modal__action--negative');
+
 
 button.addEventListener('click', () => {
   modal.classList.add('open');
@@ -27,4 +57,4 @@ function closeModal() {
   setTimeout(() => {
       backdrop.style.display = 'none';
   }, 200);
-}
+}*/
