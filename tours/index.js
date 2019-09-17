f69c60a53ad815b10e06c299be7e976cd9c561ca
melinamejia95 <melinamejia95@gmail.com>
2019-09-16 21:34:43 -0500

    Validations for form and dialog added

--- tours/index.js
+++ tours/index.js
@@ -1,107 +1,107 @@
-
-/*var button = document.querySelector('.button');
-var closeModalButton = document.querySelector('.modal__action--negative');
-var modal = document.querySelector('.modal');
 var backdrop = document.querySelector('.backdrop');
-var infoMessage = document.querySelector('#schedule-page__message');
-var form = document.querySelector('.schedule-page__form');
-var disabledHandle, tabHandle;
-
-function openModal() {
-  modal.hidden = false;
-  backdrop.hidden = false;
-  disabledHandle = ally.maintain.disabled({
-    filter: modal,
-  });
-  tabHandle = ally.maintain.tabFocus({
-    context: modal,
-  });
-}
-
-function closeModal() {
-  modal.hidden = true;
-  backdrop.hidden = true;
-  disabledHandle.disengage();
-  tabHandle.disengage();
-  infoMessage.hidden = false;
-  form.hidden = true;
-}
-
-button.addEventListener('click', openModal, false);
-closeModalButton.addEventListener('click', closeModal, false);
-*/
-
-var backdrop = document.querySelector('.backdrop');
-var modal = document.querySelector('.modal');
+var modal = document.querySelector('.modal-overlay');
 var button = document.querySelector('.button');
 var closeModalButton = document.querySelector('.modal__action--negative');
-
-
-/* button.addEventListener('click', () => {
-  modal.classList.add('open');
-  backdrop.style.display = 'block';
-        setTimeout(() => {
-            backdrop.classList.add('open');
-        }, 10);
-}); */
-
-function openModal() {
-  modal.classList.add('open');
-  backdrop.style.display = 'block';
-        setTimeout(() => {
-            backdrop.classList.add('open');
-        }, 10);
-}
-
-if (closeModalButton) {
-  closeModalButton.addEventListener('click', closeModal);
-}
-
-backdrop.addEventListener('click', () => {
-  this.closeModal();
-});
-
-function closeModal() {
-  if (modal) {
-      modal.classList.remove('open');
-  }
-  backdrop.classList.remove('open');
-  setTimeout(() => {
-      backdrop.style.display = 'none';
-  }, 200);
-}
-
+var lastFocus;
+var modalOpen = false;
 const element = document.querySelector('form');
+const elements = [
+  {
+    field: 'name',
+    labelError: 'nameError',
+    errorMessage: 'Please enter your name'
+  },
+  {
+    field: 'lastname',
+    labelError: 'lastnameError',
+    errorMessage: 'Please enter your lastname'
+  },
+  {
+    field: 'email',
+    labelError: 'emailError',
+    errorMessage: 'Make sure your email is correct'
+  },
+  {
+    field: 'city',
+    labelError: 'cityError',
+    errorMessage: 'Please select your city'
+  }
+]
+
+// restrict tab focus on elements only inside modal window
+window.addEventListener('keypress', focusRestrict);
+
+//open modal only when all fields are valid
 element.addEventListener('submit', (event) => {
   event.preventDefault();
   try {
-    const nameInput = document.querySelector('#names');
-    const labelNameError = document.querySelector('#namesError');
+    elements.map((el) => {
+      if (el.field !== "email") {
+        validateName(el.field, el.labelError, el.errorMessage);
+      } else {
+        validateEmail(el.field, el.labelError, el.errorMessage);
+      }
+    })
+    /* const nameInput = document.querySelector('#name');
+    const labelNameError = document.querySelector('#nameError');
     validateName(nameInput, labelNameError);
     const lastnameInput = document.querySelector('#lastname');
     const labelLastnameError = document.querySelector('#lastnameError');
     validateName(lastnameInput, labelLastnameError);
-    /* const emailInput = document.querySelector('#email');
-    const labelEmailError = document.querySelector('#emailError');
-    validateEmail(emailInput, labelEmailError); */
+    const emailInput = document.querySelector('#email');
+    const labelEmailError = document.querySelector('#emailError'); */
     openModal();
   }
-  catch (err){
+  catch (err) {
     console.log(err.message)
   }
-  
-})
+});
 
-function validateName(field, errorField) {
-  if(field.value === '' || field.value === undefined ) {
-    field.focus();
-    throw new Error(`Whooops!' ${field.id} field is empty`);
+function openModal() {
+  lastFocus = document.activeElement;
+  modalOpen = true;
+  modal.setAttribute("aria-hidden", false);
+  modal.setAttribute('tabindex', '0');
+  document.querySelector('#modal-title').setAttribute('tabindex', '0');
+  document.querySelector('#modal-title').focus();
+}
+
+function closeModal() {
+  modal.setAttribute("aria-hidden", true);
+  lastFocus.focus();
+}
+
+function focusRestrict(event) {
+  document.addEventListener('focus', function (event) {
+    if (modalOpen && !modal.contains(event.target)) {
+      event.stopPropagation();
+      modal.focus();
+    }
+  }, true);
+}
+
+//validate fields are not empty
+function validateName(field, errorField, message) {
+  const inputElement = document.querySelector(`#${field}`);
+  const labelElement = document.querySelector(`#${errorField}`);
+  if (inputElement.value === '' || inputElement.value === undefined) {
+    inputElement.focus();
+    labelElement.innerHTML = message;
+    throw new Error(`Whooops!' ${inputElement.id} field is empty`);
   } else {
-    errorField.hidden = true;
+    labelElement.hidden = true;
   }
 }
 
-function validateEmail(email) {
+//validate email structure
+function validateEmail(email, errorField, message) {
+  const emailInput = document.querySelector(`#${email}`);
+  const labelEmailError = document.querySelector(`#${errorField}`);
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
+  if (!re.test(emailInput.value)) {
+    labelEmailError.innerHTML = message;
+    throw new Error(`Whooops!' Email field is empty`);
+  } else {
+    labelEmailError.hidden = true;
+  }
 }
